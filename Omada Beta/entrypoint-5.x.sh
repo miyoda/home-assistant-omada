32044de4010bc27d72ea1d5d387fbec15389e27c
DraTrav <42803478+DraTrav@users.noreply.github.com>
2022-08-29 22:45:27 -0500

    Update entrypoint-5.x.sh

--- Omada Beta/entrypoint-5.x.sh
+++ Omada Beta/entrypoint-5.x.sh
@@ -138,7 +138,6 @@ if [ ! -d "${OMADA_DIR}/data/pdf" ]
 then
   # missing directory; extract from original
   echo "INFO: Report PDF directory missing; creating '${OMADA_DIR}/data/pdf'"
-  mkdir "${OMADA_DIR}/data"
   mkdir "${OMADA_DIR}/data/pdf"
   chown -R omada:omada "${OMADA_DIR}/data/pdf"
 fi
