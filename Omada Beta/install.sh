e95ef2479298b0c475877b609294b2ed8dfbf53a
DraTrav <42803478+DraTrav@users.noreply.github.com>
2023-11-03 19:34:33 -0500

    update to latest release
    updated to beta release

--- Omada Beta/install.sh
+++ Omada Beta/install.sh
@@ -6,7 +6,7 @@ OMADA_DIR="/opt/tplink/EAPController"
 ARCH="${ARCH:-}"
 OMADA_VER="${OMADA_VER:-}"
 OMADA_TAR="${OMADA_TAR:-}"
-OMADA_URL="https://static.tp-link.com/upload/software/2023/202303/20230321/Omada_SDN_Controller_v5.9.31_Linux_x64.tar.gz"
+OMADA_URL="https://static.tp-link.com/upload/beta/2023/202310/20231027/Omada_SDN_Controller_v5.13.10_Linux_x64.tar.gz(Beta).gz"
 OMADA_MAJOR_VER="$(echo "${OMADA_VER}" | awk -F '.' '{print $1}')"
 
 
