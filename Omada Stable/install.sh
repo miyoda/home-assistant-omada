97a9bfb8f95ab1bdf22ef40e3aa4740bd0cee365
Miguel Ángel Hernández Castro <miyoda@gmail.com>
2024-02-09 14:00:15 +0100

    Update install.sh to controller version 5.13.23
    More info https://www.tp-link.com/es/support/download/omada-software-controller/#Controller_Software

--- Omada Stable/install.sh
+++ Omada Stable/install.sh
@@ -6,7 +6,7 @@ OMADA_DIR="/opt/tplink/EAPController"
 ARCH="${ARCH:-}"
 OMADA_VER="${OMADA_VER:-}"
 OMADA_TAR="${OMADA_TAR:-}"
-OMADA_URL="https://static.tp-link.com/upload/beta/2023/202310/20231027/Omada_SDN_Controller_v5.13.10_Linux_x64.tar.gz(Beta).gz"
+OMADA_URL="https://static.tp-link.com/upload/software/2024/202401/20240112/Omada_SDN_Controller_v5.13.23_linux_x64.tar.gz"
 OMADA_MAJOR_VER="$(echo "${OMADA_VER}" | awk -F '.' '{print $1}')"
 
 
